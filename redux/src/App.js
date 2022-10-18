3e09205688fa4c0e5d738dbdfc4eadaf5c19b54d
santhosh <santhoshkaliannann@gmail.com>
2022-10-18 15:17:48 +0530

    Selector Name Changed

--- redux/src/App.js
+++ redux/src/App.js
@@ -3,11 +3,11 @@ import {useDispatch,useSelector} from 'react-redux';
 import {VadivelActions,GounderActions} from './index';
 
 function App() {
-  const comedies = useSelector(state=>state);
+  const comedy = useSelector(state=>state);
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <p>{comedies}</p>
+      <p>{comedy}</p>
       <button onClick={()=>dispatch(VadivelActions())}>Vadivel</button>&nbsp;
       <button onClick={()=>dispatch(GounderActions())}>Gounder</button>
       
