a1160dc9cfc09196ad3a4a1ceb7e05f5cb59282b
santhosh <santhoshkaliannann@gmail.com>
2022-10-19 13:00:28 +0530

    change all files into proper file format

--- redux/src/index.js
+++ redux/src/index.js
@@ -3,32 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {createStore} from 'redux';
 import { Provider } from 'react-redux';
+import ComedyReducer from './reducer/reducer';
+
 
 //selector
-const VADIVEL_COMEDY ='Vadivel_Comedy';
-const GOUNDAMANI_COMEDY ='Goundamani_Comedy';
+
 //Action
-export const VadivelActions=()=>{
-  return({
-    type:VADIVEL_COMEDY
-  })
-}
-export const GounderActions=()=>{
-  return({
-    type:GOUNDAMANI_COMEDY
-  })
-}
+
 //Reducer
-const ComedyReducer =(state,actions)=>{
-  switch(actions.type){
-    case VADIVEL_COMEDY:
-      return state="He is Good"
-      case GOUNDAMANI_COMEDY:
-      return state="He is Bad"
-      default:
-        return state="No Comments"
-  }
-}
+
 let store = createStore(ComedyReducer);
 
 
