bcf6f69aefe179cba9bb810812f441d336cdd6ee
santhosh <santhoshkaliannann@gmail.com>
2022-10-19 13:01:17 +0530

    create a new file for actions

---
+++ redux/src/action/action.js
@@ -0,0 +1,11 @@
+import * as constant from "../constants/constants"
+export const VadivelActions = () => {
+    return ({
+        type: constant.VADIVEL_COMEDY
+    })
+}
+export const GounderActions = () => {
+    return ({
+        type: constant.GOUNDAMANI_COMEDY
+    })
+}
